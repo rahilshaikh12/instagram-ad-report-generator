0481bf4f65a0452157c9b258993bef7b909d5b4f
Rahil Nabi Shaikh <rahilnabi19@gmail.com>
2025-04-08 14:29:18 -0400

    add section 2

--- LaTeX.R
+++ LaTeX.R
@@ -50,6 +50,15 @@ This section presents a donut chart that shows the proportion of advertisers tha
     \\caption{Advertisers with access to your personal information.}
 \\end{figure}
 
+\\section*{Interaction History}
+This graph represents the number of advertisers with whom you have directly interacted---such as visiting their websites or making purchases. It illustrates your direct engagement level with these companies by dividing them into groups based on your interaction history.
+
+\\begin{figure}[H]
+    \\centering
+    \\includegraphics[width=0.8\\textwidth]{plot_interaction.png}
+    \\caption{Advertisers you've interacted with before.}
+\\end{figure}
+
 
 
 
