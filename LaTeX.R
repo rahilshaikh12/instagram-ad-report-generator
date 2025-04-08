7f3b6dab2160fba6693f10c6efc84dc73a271705
Rahil Nabi Shaikh <rahilnabi19@gmail.com>
2025-04-08 14:26:36 -0400

    changes to LaTeX.R

---
+++ LaTeX.R
@@ -0,0 +1,19 @@
+# generate_dashboard_tex.R
+
+# necessary imports
+latex_code <- "
+\\documentclass[a4paper,12pt]{article}
+\\usepackage[utf8]{inputenc}
+\\usepackage[T1]{fontenc}
+\\usepackage[margin=1in]{geometry}
+\\usepackage{graphicx}
+\\usepackage{float}
+\\usepackage{caption}
+\\usepackage{subcaption}
+\\usepackage{fancyhdr}
+\\usepackage{xcolor}
+\\usepackage{titlesec}
+
+
+
+"
