9342a7f1e40ec5d897794bcb99419eb18b6d5f45
Rahil Nabi Shaikh <rahilnabi19@gmail.com>
2025-04-08 13:57:45 -0400

    changes to main R file

--- code.R
+++ code.R
@@ -1,3 +1,19 @@
 library(tidyverse)
 library(rjson)
 library(UpSetR)
+
+# converting the JSON data to a data frame
+ad_data <- fromJSON(file = "advertisers_using_your_activity_or_information.json")
+
+ad_data <- ad_data[[1]]
+ad_data <- do.call(rbind, lapply(ad_data, function(data_list) {
+  data.frame(t(unlist(data_list)), stringsAsFactors = FALSE)
+}))
+
+# changing column names for better readability 
+
+colnames(ad_data) <- c("company",
+                       "has_personal_info",
+                       "interacted_with_before",
+                       "visited_store")
+
